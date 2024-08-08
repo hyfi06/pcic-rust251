72c6fca92c4b73685aaa412cb99467b48e7878df
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-08-08 11:09:55 -0600

    funciones
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let n: u32 = 10;
-    let m: u64 = 10;
-    println!("{}", n + m); // cannot add `u64` to `u32
+    let n = 10i64;
+    println!("{}", duplica(n));
+}
+
+fn duplica(x: i64) -> i64 {
+    return x + x;
 }

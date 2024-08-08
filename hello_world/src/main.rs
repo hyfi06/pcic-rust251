e0591a6fd8f2d8c6bb2cb1a122af2408e25c78b2
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-08-08 11:33:32 -0600

    referencia de memoria
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     let n = 10.5f64;
+    let s1 = String::from("hola mundo");
+    println!("{}", s1);
+    let s2 = &s1;
+    println!("{}", s2);
+    println!("{}", s1);
     println!("{}", duplica(n));
 }
 

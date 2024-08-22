048b1c2ec06a5fe14f038ea37b8e187ac0c67ccd
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-08-22 08:46:36 -0600

    ejercicios
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ copy_clone_move/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let n = 10;
+    let r = &n;
+    let m = r + 1;
+    println("{}", m);
+
+    let v = vec![1,2,3];
+}
---
+++ distancia/src/main.rs
@@ -0,0 +1,73 @@
+use std::ops::Deref;
+
+fn main() {}
+
+fn distance(p: &Point, q: &Point) -> f64 {
+    p.distance(q)
+}
+
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    fn distance(&self, q: &Point) -> f64 {
+        ((self.x - q.x).powi(2) + (self.y - q.y).powi(2)).sqrt()
+    }
+}
+
+struct ColoredPoint {
+    point: Point,
+    color: u32,
+    temp: Option<f64>,
+}
+
+impl Deref for ColoredPoint {
+    type Target = Point;
+    fn deref(&self) -> &Self::Target {
+        &self.point
+    }
+}
+
+impl AsRef<u32> for ColoredPoint {
+    fn as_ref(&self) -> &u32 {
+        &self.color
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn points() {
+        let p = Point { x: 0.0, y: 4.0 };
+        let q = Point { x: 3.0, y: 0.0 };
+        assert_eq!(distance(&p, &q), 5.0);
+    }
+
+    #[test]
+    fn colored_points() {
+        let p = ColoredPoint {
+            point: Point { x: 0.0, y: 4.0 },
+            color: 1,
+        };
+        let q = ColoredPoint {
+            point: Point { x: 3.0, y: 0.0 },
+            color: 2,
+        };
+        assert_eq!(distance(&p, &q), 5.0);
+    }
+
+    #[test]
+    fn test_color() {
+        let p = ColoredPoint {
+            point: Point { x: 0.0, y: 4.0 },
+            color: 1,
+        };
+        let n: &u32 = p.as_ref();
+        println!("Color: {}", n);
+        assert_eq!(n, &1);
+    }
+}

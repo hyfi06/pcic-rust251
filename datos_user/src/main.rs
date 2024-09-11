6eaf63fb2837d9657f223a9db7235dbf2c766a80
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-09-11 11:05:52 -0600

    Ejercio de entrada del usuario
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- datos_user/src/main.rs
+++ datos_user/src/main.rs
@@ -3,9 +3,19 @@ fn main() {
     let mut name: String = String::new();
     std::io::stdin().read_line(&mut name).unwrap();
     name = name.trim().to_string();
+
     println!("Por favor introduce tu edad: ");
     let mut edad: String = String::new();
     std::io::stdin().read_line(&mut edad).unwrap();
     let edad_int: u8 = edad.trim().parse().unwrap();
-    println!("Hola, bienvenido o bienvenida {} de {} años", name, edad_int);
+
+    println!("Por favor introduce tu país: ");
+    let mut pais: String = String::new();
+    std::io::stdin().read_line(&mut pais).unwrap();
+    pais = pais.trim().to_string();
+
+    println!(
+        "Hola, bienvenido o bienvenida {} de {} años, proveniente de {}",
+        name, edad_int, pais
+    );
 }

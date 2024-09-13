a56f92586d05b7b3a3e5761405fe1549134a5b70
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-09-12 22:11:10 -0600

    ejercio de temperatura
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ mostrar_temperatura/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let temp_min: i16 = -5;
+    let temp_max: i16 = 37;
+
+    println!(
+        "Temperatura mínima: {} \nTemperatura máxima: {}",
+        temp_min, temp_max
+    );
+}

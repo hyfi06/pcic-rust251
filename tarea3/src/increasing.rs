0a7f927749ea584185c7049473f92bf3f0266d5f
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-09-19 21:54:59 -0600

    clase 20240919
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- tarea3/src/increasing.rs
+++ tarea3/src/increasing.rs
@@ -1,19 +1,16 @@
 pub mod generic;
 pub mod u32;
 
-pub trait Increasing
-where
-    Self: Iterator + 'static,
-    Self::Item: PartialOrd,
-{
-    fn increasing(self) -> Box<dyn Iterator<Item = Self::Item>> where Self:Sized, <Self as Iterator>::Item: 'static {
-        Box::new(generic::increasing_generic(self))
-    }
+pub trait Increasing: Iterator {
+    fn increasing(self) -> impl Iterator<Item = Self::Item>;
 }
 
 impl<I> Increasing for I
 where
-    I: Iterator + 'static,
+    I: Iterator,
     I::Item: PartialOrd,
 {
+    fn increasing(self) -> impl Iterator<Item = Self::Item> {
+        generic::increasing_generic(self)
+    }
 }

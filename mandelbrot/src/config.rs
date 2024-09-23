b79747c9e2ea39cded491000b47845e48ecae25f
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-09-22 22:21:52 -0600

    refactor mandelbrot
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ mandelbrot/src/config.rs
@@ -0,0 +1,5 @@
+pub const DEFAULD_RESOLUTION: f64 = 4.0 / 1000.0;
+pub const DEFAULT_X_MAX: f64 = 2.0;
+pub const DEFAULT_X_MIN: f64 = -2.0;
+pub const DEFAULT_Y_MAX: f64 = 2.0;
+pub const DEFAULT_Y_MIN: f64 = -2.0;
